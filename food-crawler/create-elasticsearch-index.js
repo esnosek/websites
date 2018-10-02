c48a01590845c0bc3246852c272e082750db0fe0
esnosek <esnosek@gmail.com>
2018-10-02 15:27:31 +0200

    create elasticsearch index with data

---
+++ food-crawler/create-elasticsearch-index.js
@@ -0,0 +1,40 @@
+const elasticsearch = require('elasticsearch');
+const glob = require("glob");
+var fs = require('fs');
+
+const client = new elasticsearch.Client({
+  host: 'localhost:9200',
+  log: 'debug'
+});
+
+async function createIndexAndInsertData(){
+  await createIndex()
+  addProducts()
+}
+
+async function createIndex(){
+  client.indices.create({index: 'food-index'}, (err, res, status) => {
+    if(err) throw err;
+    console.log("Create", res);
+  });
+}
+
+function addProducts(){
+  glob("data/**/*.json", {matchBase:true}, (err, res) => {
+    if (err) throw err;
+    addBulk(res)
+  });
+}
+
+function addBulk(files){
+  const data = []
+  files.forEach(f => {
+    data.push({ index:  { _index: 'food-index', _type: 'product'} });
+    data.push(JSON.parse(fs.readFileSync(f, 'utf8')))
+  });
+  client.bulk({
+    body : JSON.parse(JSON.stringify(data))
+  })
+}
+
+createIndexAndInsertData()
