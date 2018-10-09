001894298080ceaef0d57809afd0fa31cb118997
esnosek <esnosek@gmail.com>
2018-10-09 14:50:11 +0200

    add product quantity calculation

--- food-crawler/server.js
+++ food-crawler/server.js
@@ -14,45 +14,51 @@ app.use('/styles', express.static(__dirname + "/styles"));
 const port = 80;
 const todaysProducts = [];
 const todayToEat = {
-    energy: 3340,
-    protein: 170,
-    carbohydrates: 530,
-    fat: 60
+    energy: 3340.0,
+    protein: 170.0,
+    carbohydrates: 530.0,
+    fat: 60.0
 }
 
 const eatenToday = {
-    energy: 0,
-    protein: 0,
-    carbohydrates: 0,
-    fat: 0
-}
-
-const allValues = {
-    quantity : 0,
-    energy : 0,
-    protein : 0,
-    carbohydrates : 0,
-    fat : 0
+    quantity: 0,
+    energy: 0.0,
+    protein: 0.0,
+    carbohydrates: 0.0,
+    fat: 0.0
 }
 
 function addProduct(product, quantity){
-    product.quantity = parseInt(quantity);
-    todaysProducts.push(product);
-    allValues.quantity += product.quantity
-    allValues.energy += product.nutritionalValues.energy;
-    allValues.protein += product.nutritionalValues.protein;
-    allValues.carbohydrates += product.nutritionalValues.carbohydrates;
-    allValues.fat += product.nutritionalValues.fat;
+    productValues = calculateProductValues(product, quantity);
+    todaysProducts.push(productValues);
+    updateEatenToday(productValues);
+    updateTodayToEat(productValues);
+}
 
-    eatenToday.energy += product.nutritionalValues.energy;
-    eatenToday.protein += product.nutritionalValues.protein;
-    eatenToday.carbohydrates += product.nutritionalValues.carbohydrates;
-    eatenToday.fat += product.nutritionalValues.fat;
+function calculateProductValues(product, quantity){
+    return {
+        name : product.productName,
+        quantity: quantity,
+        energy : product.nutritionalValues.energy * 1000 * quantity / (100 * 1000),
+        protein : product.nutritionalValues.protein * 1000 * quantity / (100 * 1000),
+        carbohydrates : product.nutritionalValues.carbohydrates * 1000 * quantity / (100 * 1000),
+        fat : product.nutritionalValues.fat * 1000 * quantity / (100 * 1000)
+    }
+}
 
-    todayToEat.energy -= product.nutritionalValues.energy;
-    todayToEat.protein -= product.nutritionalValues.protein;
-    todayToEat.carbohydrates -= product.nutritionalValues.carbohydrates;
-    todayToEat.fat -= product.nutritionalValues.fat;
+function updateEatenToday(product){
+    eatenToday.quantity = Math.round((eatenToday.quantity + productValues.quantity) * 100) / 100;
+    eatenToday.energy = Math.round((eatenToday.energy + productValues.energy) * 100) / 100;
+    eatenToday.protein = Math.round((eatenToday.protein + productValues.protein) * 100) / 100;
+    eatenToday.carbohydrates = Math.round((eatenToday.carbohydrates + productValues.carbohydrates) * 100) / 100;
+    eatenToday.fat = Math.round((eatenToday.fat + productValues.fat) * 100) / 100;
+}
+
+function updateTodayToEat(product){
+    todayToEat.energy = Math.round((todayToEat.energy - productValues.energy) * 100) / 100;
+    todayToEat.protein = Math.round((todayToEat.protein - productValues.protein) * 100) / 100;
+    todayToEat.carbohydrates = Math.round((todayToEat.carbohydrates - productValues.carbohydrates) * 100) / 100;
+    todayToEat.fat = Math.round((todayToEat.fat - productValues.fat) * 100) / 100;
 }
 
 app.get('/', (req, res) => {
@@ -60,8 +66,7 @@ app.get('/', (req, res) => {
         todaysProducts: todaysProducts, 
         foundProducts: [], 
         todayToEat: todayToEat,
-        eatenToday: eatenToday,
-        allValues: allValues
+        eatenToday: eatenToday
     });
 });
 
@@ -72,8 +77,7 @@ app.get('/product', (req, res) => {
                 todaysProducts: todaysProducts, 
                 foundProducts: r.hits.hits.map(h => h._source), 
                 todayToEat: todayToEat,
-                eatenToday: eatenToday,
-                allValues: allValues
+                eatenToday: eatenToday
             });
         });
 });
@@ -89,13 +93,12 @@ app.post('/addProduct', (req, res) => {
     productRepository.findByProductName(req.body.productName)
         .then(r => {
             const product = r.hits.hits[0]._source
-            addProduct(product, req.body.quantity)
+            addProduct(product, parseInt(req.body.quantity))
             res.render('index', { 
                 todaysProducts: todaysProducts, 
                 foundProducts: [], 
                 todayToEat: todayToEat,
-                eatenToday: eatenToday,
-                allValues: allValues
+                eatenToday: eatenToday
             });
         });
 });
